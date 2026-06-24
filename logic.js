bcef4b094314d022fecd024f43855dab847d38cc
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-06-24 22:41:14 +0530

    adding pictures of moves

--- logic.js
+++ logic.js
@@ -7,3 +7,13 @@ let rightFist = document.querySelector("#right-fist1");
 let playerScore = document.querySelector("#player-score");
 let computerScore = document.querySelector("#computer-score");
 let symbolsContainer = document.querySelector(".symbols");
+let stoneBtn = document.querySelector("#stone");
+let paperBtn = document.querySelector("#paper");
+let scissorBtn = document.querySelector("#scissor");
+let newGameBtn = document.querySelector("#new-btn");
+
+function leftFistMove(){
+    leftFist.addEventListener("click", ()=>{
+        https://i.pinimg.com/474x/67/eb/fd/67ebfd32e898b4ec3ae4e59ae33832c8.jpg
+    })};
+leftFistMove();
